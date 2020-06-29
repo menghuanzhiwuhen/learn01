eb309b5fecd448b61853970b711c5896399e8ca6
zhangyingjie <1242576887@qq.com>
2020-06-29 16:19:15 +0800

    first commit

---
+++ src/test/java/com/example/learn01/Learn01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learn01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Learn01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
